613e92f42b2ec091c3ad6ec1f5acff3c500cd0d9
davidbreenwex <davidbreenwex@gmail.com>
2017-03-06 16:21:16 -0800

    Add User Sample Id Example

---
+++ samples/Objective-C/UserSampleIdExample/UserSampleIdExample/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  UserSampleIdExample
+//
+//  Created by David Breen on 2/28/17.
+//  Copyright © 2017 swrve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *userIdLabel;
+@property (weak, nonatomic) IBOutlet UILabel *userSampleIdLable;
+
+
+@end
+
